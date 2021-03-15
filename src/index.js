075c0ca805b7592062e375b3dda0088aa8c97219
SverdlovRodion <78374115+SverdlovRodion@users.noreply.github.com>
2021-03-16 00:29:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  var max = arr[0];
+for(var i=1; i<arr.length; i++){
+if(arr[i] > max){
+max = arr[i];
+}
+}
+return max;
 }
 
 exports.max = function max (array) {
-  return 0;
+  var min = arr[0];
+for(var i=1; i<arr.length; i++){
+if(arr[i] < min){
+min = arr[i];
+}
+}
+return min;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  var avg;
+var sum = 0;
+for(var i=0; i<arr.length; i++){
+sum+=arr[i];
+}
+return avg=sum/arr.length;
 }
